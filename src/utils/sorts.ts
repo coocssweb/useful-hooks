64d2aa88b6d2b1e5d4cb9a07c4b7a2907f871932
wangjiaxin <wangjiaxin@leedarson.com>
2020-07-30 11:26:49 +0800

    add quick sort

--- src/utils/sorts.ts
+++ src/utils/sorts.ts
@@ -1,8 +1,8 @@
+const sortAsc = (a, b) => a > b;
+const sortDesc = (a, b) => a < b;
 // 冒泡排序
 const bubbleSort = (unSorted, isAsc = true) => {
   const result = [...unSorted];
-  const sortAsc = (a, b) => a > b;
-  const sortDesc = (a, b) => a < b;
   const compare = isAsc ? sortAsc : sortDesc;
 
   // 3, 5, 9, 4, 1
@@ -27,8 +27,6 @@ const bubbleSort = (unSorted, isAsc = true) => {
 const pickSort = (unSorted, isAsc = true) => {
   const result = [];
   const targetSorted = [...unSorted];
-  const sortAsc = (a, b) => a > b;
-  const sortDesc = (a, b) => a < b;
   const compare = isAsc ? sortAsc : sortDesc;
 
   // [], [3, 5, 9, 4, 1]
@@ -41,6 +39,7 @@ const pickSort = (unSorted, isAsc = true) => {
     let a = targetSorted[0];
     let index = 0;
     for (let j = 1; j < targetSorted.length; j += 1) {
+      console.log('===================');
       if (compare(a, targetSorted[j])) {
         index = j;
         a = targetSorted[j];
@@ -48,20 +47,50 @@ const pickSort = (unSorted, isAsc = true) => {
     }
     targetSorted.splice(index, 1);
     result.push(a);
-    console.log(result, targetSorted);
+    // console.log(result, targetSorted);
   }
 
   return result;
 };
 
 // 快速排序
-// const quickSort = (unSorted, isAsc = true) => {};
+const quickSort = (unSorted, isAsc = true) => {
+  if (unSorted.length <= 1) {
+    return unSorted;
+  }
+  const compare = isAsc ? sortAsc : sortDesc;
+  // [3, 5, 9, 4, 1]
+  // [1], 3, [5, 9, 4]
+  // [1], 3, [4], 5, [9]
+  const targetValue = unSorted[0];
+  const left = [];
+  const right = [];
+  for (let j = 1; j < unSorted.length; j += 1) {
+    if (compare(targetValue, unSorted[j])) {
+      left.push(unSorted[j]);
+    } else {
+      right.push(unSorted[j]);
+    }
+  }
+
+  const finalLeft = quickSort(left, isAsc);
+  const finalRight = quickSort(right, isAsc);
+
+  return [...finalLeft, targetValue, ...finalRight];
+};
 
 const numbers = [3, 5, 9, 4, 1];
+// console.log('===============================bubble sort asc');
 // console.log(bubbleSort(numbers));
-// console.log('===============================');
+// console.log('===============================bubble sort desc');
 // console.log(bubbleSort(numbers, false));
 
-console.log(pickSort(numbers));
-console.log('===============================');
-console.log(pickSort(numbers, false));
+// console.log('===============================pick sort asc');
+// console.log(pickSort(numbers));
+// console.log('===============================pick sort desc');
+// console.log(pickSort(numbers, false));
+
+// console.log('===============================quick sort asc');
+// console.log(quickSort(numbers));
+// console.log('===============================quick sort desc');
+// console.log(quickSort(numbers, false));
